cb3a48ec4015c04060120305c0202e14e303acc8
Anatolii <anatolii.lukavenko@gmail.com>
2019-02-12 19:13:05 +0200

    Homework_8: simplified algorithm for reverse task

--- FLX_homework_8/homework/js/reverseNumber.js
+++ FLX_homework_8/homework/js/reverseNumber.js
@@ -1,7 +1,10 @@
-
 function reverseNumber(num) {
 	if (Number.isInteger(num)) {
-		let reversed = num.toString().split('').reverse().join(''); 
+		let string = String(num);
+		let reversed = "";
+		for (let i = string.length - 1; i >= 0 ; i--) {
+			reversed += string[i];
+		}
 		return Math.sign(num) * parseInt(reversed); 
 	} else {
 		return "Not a number"
