c2879489ad843119f8248df70ce7b3b7881e294a
Anatolii <anatolii.lukavenko@gmail.com>
2019-02-13 11:57:46 +0200

    Homework_9: task#1

--- FLX_homework_9/homework/index.js
+++ FLX_homework_9/homework/index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+/* Task#1 */
+function findTypes() {
+	let res = [];
+	for (let i = 0; i < arguments.length; i++) {
+		 res.push(typeof arguments[i]);
+	}
+	return res;
+}
+/* Task#1 */
