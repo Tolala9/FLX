a09e9d54ba6af6bfb2db032682a018db01be23c0
Anatolii <anatolii.lukavenko@gmail.com>
2019-02-13 14:30:21 +0200

    Homework_9: task #2 upd for #3 and #4

--- FLX_homework_9/homework/index.js
+++ FLX_homework_9/homework/index.js
@@ -2,17 +2,35 @@
 function findTypes() {
 	let res = [];
 	for (let i = 0; i < arguments.length; i++) {
-		 res.push(typeof arguments[i]);
+		res.push(typeof arguments[i]);
 	}
 	return res;
 }
 
 /* Task #2 */
-function executeforEach() {
-	let arr = arguments[0];
-	let func = arguments[1];
+function executeforEach(arr, func) {
+	let res = [];
 	for (var i = 0; i < arr.length; i++) {
-		func(arr[i]);
+		let elem = func(arr[i]);
+		if (findTypes(elem) !== "undefined" )  {
+			if (elem) {
+				res.push(arr[i]);
+			} 
+		} 
 	}
+	return res;
 }
+executeforEach([1,2,3], function(el) { console.log(el) })
+
+
+/* Task #3 */
+function mapArray(x, y) {
+	return executeforEach(x, y);
+}
+
+/* Task #4 */
+function filterArray(x, y) {
+	return executeforEach(x, y);
+}	
+
 
