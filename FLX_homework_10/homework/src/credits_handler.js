d0795037860bf930307947305302affb96078c4a
Tolala9 <anatolii.lukavenko@gmail.com>
2019-02-18 23:45:17 +0200

    Homework_10: putCredits method

--- FLX_homework_10/homework/src/credits_handler.js
+++ FLX_homework_10/homework/src/credits_handler.js
@@ -8,7 +8,17 @@ function userCard(index) {
 	const cardMethods = {
 		getCardOptions: function() {
 			return { balance, transactionLimit, historyLogs, key };
+		},
+
+		putCredits: function(amount) {
+			balance += amount;
 		}
+
 	}
 	return cardMethods;
 }
+
+
+let card3 = userCard(3);
+card3.getCardOptions();
+card3. putCredits(150);
