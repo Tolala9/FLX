861946ce5e68ee7cbebdb4e03da5e0b145d5cad5
Tolala9 <anatolii.lukavenko@gmail.com>
2019-02-18 23:20:46 +0200

    Homework_10: getCardOption method

--- FLX_homework_10/homework/src/credits_handler.js
+++ FLX_homework_10/homework/src/credits_handler.js
@@ -1,1 +1,14 @@
-/* Your code goes here */
+function userCard(index) {
+	let key = index;
+	let	balance = 100;
+	let transactionLimit = 100;
+	let historyLogs = [];  
+
+
+	const cardMethods = {
+		getCardOptions: function() {
+			return { balance, transactionLimit, historyLogs, key };
+		}
+	}
+	return cardMethods;
+}
