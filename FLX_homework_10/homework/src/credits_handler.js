d4ff99aa2049085df367becb96a18376eae15895
Anatolii <anatolii.lukavenko@gmail.com>
2019-02-19 11:42:20 +0200

    Homework_10: historyLogWrite

--- FLX_homework_10/homework/src/credits_handler.js
+++ FLX_homework_10/homework/src/credits_handler.js
@@ -4,17 +4,25 @@ function userCard(index) {
 	let transactionLimit = 100;
 	let historyLogs = [];  
 	let taxes = 0.005;
+	
 
+	function historyLogWrite(operationType, credits) {
+		let time = new Date().toLocaleString('en-GB');
+		historyLogs.push({
+			operationType: operationType,
+			credits: credits,
+			operationTime: time
+		})
+	}
 
 	const cardMethods = {
 		getCardOptions: function() {
 			return { balance, transactionLimit, historyLogs, key };
 		},
-
 		putCredits: function(amount) {
 			balance += amount;
+			historyLogWrite("Received credits", amount);
 		},
-
 		takeCredits: function(amount) {
 			if (transactionLimit < amount ) {
 				console.error("Your transaction limit of card is exhausted!");
@@ -22,13 +30,13 @@ function userCard(index) {
 				console.error("Your card balance is exhausted!");
 			} else {
 				balance -= amount;
+				historyLogWrite("Withdrawal of credits", amount);
 			}
 		},
-
 		setTransactionLimit: function(amount) {
 			transactionLimit = amount;
+			historyLogWrite("Transaction limit change", amount);
 		},
-
 		transferCredits: function(amount, receiverCard) {
 			let transferAmount = (amount * taxes) + amount;
 			if (transactionLimit < transferAmount ) {
@@ -46,12 +54,21 @@ function userCard(index) {
 }
 
 
-let card3 = userCard(3);
-card3.getCardOptions();
-card3.putCredits(150);
-card3.takeCredits(50);
-card3.setTransactionLimit(500);
+// let card3 = userCard(3);
+// console.log(card3.getCardOptions());
+// card3.putCredits(150);
+// card3.takeCredits(50);
+// card3.setTransactionLimit(500);
+// console.log(card3.getCardOptions());
 
-let card3 = userCard(3);
-let card1 = userCard(1);
-card1.transferCredits(100, card3);
+
+// card3.takeCredits(50);
+// card3.setTransactionLimit(500);
+
+// let card3 = userCard(3);
+
+// let card1 = userCard(1);
+// card1.transferCredits(50, card3);
+// console.log(card3.getCardOptions());
+// card1.transferCredits(30, card3);
+// console.log(card3.getCardOptions());
