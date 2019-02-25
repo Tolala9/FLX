16b7e7711fde854e24e516ee4b995f8a44607afd
Anatolii <anatolii.lukavenko@gmail.com>
2019-02-25 17:31:46 +0200

    Homework_12: adding new task and show list of tasks

--- FLX_homework_12/homework/src/app.js
+++ FLX_homework_12/homework/src/app.js
@@ -4,22 +4,13 @@ const rootNode = document.getElementById('root');
 //     {isDone: false, id: 12345, description: 'Todo 1'}
 // ];
 
-// rootNode.appendChild(/* Append your list item node*/);
-
 /* Creating HTML layout */
 rootNode.classList.add('app-wrapper');
 const listPageHTML = `
 	<div id="page-list" class="list-page">
 		<h1 class="page-title">Simple TODO aplication</h1>
 		<button id="btn-add--new" class="btn">Add new task</button>
-		<ul id="task-list" class="task-list">
-			<p class="empty-list">TODO is empty</p>
-			<li class="task-list__item">
-				<img class="item-status" src="assets/img/todo-s.png" alt="checkbox">
-				<a class="item-text" href="#">Hello</a>
-				<img class="item-delete" src="assets/img/remove-s.jpg" alt="delete button">
-			</li>
-		</ul>
+		<ul id="task-list" class="task-list"> </ul>
 	</div>`;
 rootNode.insertAdjacentHTML('beforeend', listPageHTML);
 const addTaskPageHTML = `
@@ -28,7 +19,7 @@ const addTaskPageHTML = `
 		<input id="add-new-input" class="input" type="text" >
 		<div class="btns-wrapper">
 			<button id="btn-cancel" class="btn">Cancel</button>
-			<button id="btn-add-item" class="btn">Save changes</button>
+			<button id="btn-add-item" class="btn" disabled >Save changes</button>
 		</div>
 	</div>`;
 rootNode.insertAdjacentHTML('beforeend', addTaskPageHTML);
@@ -42,20 +33,34 @@ const modifyTaskPageHTML = `
 		</div>
 	</div>`;
 rootNode.insertAdjacentHTML('beforeend', modifyTaskPageHTML);
+/* End Creating HTML layout */
+
+/* Main variables */
+let tasks = [];
 
 const listPage = document.querySelector('#page-list');
 const addTaskPage = document.querySelector('#page-add-task');
 const modifyTaskPage = document.querySelector('#page-modify-task');
+let addNewInput = document.querySelector('#add-new-input');
+const btnAdd = document.querySelector('#btn-add--new');
+const btnAddItem = document.querySelector('#btn-add-item');
+const taskList = document.querySelector('#task-list');
 
-// location.hash = '#/add';
-// location.hash = '#/modify/';
-// location.hash = '#/add';
+/* App execution */
+window.onload = function () {
+	hashServ();
+	window.onhashchange = hashServ;
+	checkInputField();
+	showTaskList(tasks);
+	saveNewTask();
+}
 
-
+/* Serving hash in links */
 function hashServ() {
 	listPage.classList.add('hidden');
 	addTaskPage.classList.add('hidden');
 	modifyTaskPage.classList.add('hidden');
+
 	if (location.hash === '#/add') {
 		addTaskPage.classList.remove('hidden');
 	} else if(location.hash === '#/modify/') {
@@ -64,5 +69,75 @@ function hashServ() {
 		listPage.classList.remove('hidden');
 	}
 }
+btnAdd.onclick = (e) => {
+	location.hash = '#/add';
+}
 
-hashServ();
+
+function checkInputField() {
+	addNewInput.onkeyup = function() {
+		if (addNewInput.value.trim()) {
+			btnAddItem.disabled = false;
+		} else {
+			btnAddItem.disabled = true;
+		}
+	}
+}
+
+function saveNewTask() {
+	btnAddItem.onclick = function() {
+	addNewInput = document.querySelector('#add-new-input');
+	location.hash = '';
+	addTask(addNewInput.value);
+	showTaskList(tasks);
+	addNewInput.value = '';
+	}
+}
+
+function addTask(text) {
+	let indexItem = tasks.length ;
+	tasks.push({
+		isDone: false,
+		id: 'task_' + ++indexItem,
+		description: text  
+	});
+}
+
+function showTask(task) {
+	let newTask = document.createElement('li');
+	newTask.classList.add('task-list__item');
+	newTask.dataset.id = task.id;
+	if(task.isDone) {
+		newTask.classList.add('done');
+		newTask.innerHTML += `<img class="item-status" 
+		src="assets/img/done-s.png" alt="checkbox">`;
+		newTask.innerHTML += `<a class="item-text" 
+		href="#">${task.description}</a>`;
+		newTask.innerHTML += `<img class="item-delete" 
+		src="assets/img/remove-s.jpg" alt="delete button">`;
+	} else {
+		newTask.innerHTML += `<img class="item-status" 
+		src="assets/img/todo-s.png" alt="checkbox">`;
+		newTask.innerHTML += `<a class="item-text" 
+		href="#">${task.description}</a>`;
+		newTask.innerHTML += `<img class="item-delete" 
+		src="assets/img/remove-s.jpg" alt="delete button">`;
+	}
+
+	return newTask;
+}
+
+function showTaskList(tasks) {
+	taskList.innerHTML = '';
+	if (tasks && tasks.length) {
+		tasks.forEach( function(task) {
+			let newTask = showTask(task);
+			taskList.appendChild(newTask);
+		});
+	} else {
+		taskList.innerHTML = `<p class="empty-list">TODO is empty</p>`;
+	}
+}
+
+
+
