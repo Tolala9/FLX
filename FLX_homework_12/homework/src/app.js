fd968dffc13b91a3cb70e4a95dd661e03ce5b2f1
Anatolii <anatolii.lukavenko@gmail.com>
2019-02-26 17:41:58 +0200

    Homework_12: cancel btn action & hash adjusting for updating task desc

--- FLX_homework_12/homework/src/app.js
+++ FLX_homework_12/homework/src/app.js
@@ -44,6 +44,7 @@ let addNewInput = document.querySelector('#add-new-input');
 const btnAdd = document.querySelector('#btn-add--new');
 const btnAddItem = document.querySelector('#btn-add-item');
 const taskList = document.querySelector('#task-list');
+const btnCancel = document.querySelectorAll('#btn-cancel');
 
 /* App execution */
 window.onload = function () {
@@ -73,14 +74,16 @@ function saveToNewData(items) {
 
 /* Serving hash in links */
 function hashServ() {
+	const hash = location.hash;
 	listPage.classList.add('hidden');
 	addTaskPage.classList.add('hidden');
 	modifyTaskPage.classList.add('hidden');
 
 	if (location.hash === '#/add') {
 		addTaskPage.classList.remove('hidden');
-	} else if(location.hash === '#/modify/') {
+	} else if((/\/modify\/task_\d+$/).test(hash)) {
 		modifyTaskPage.classList.remove('hidden');
+		
 	} else {
 		listPage.classList.remove('hidden');
 	}
@@ -89,6 +92,11 @@ btnAdd.onclick = (e) => {
 	location.hash = '#/add';
 }
 
+btnCancel.forEach((item) => {
+    item.onclick = function(e) {
+        location.hash = '';
+    }
+});
 
 function checkInputField() {
 	addNewInput.onkeyup = function() {
@@ -129,15 +137,15 @@ function showTask(task) {
 		newTask.classList.add('done');
 		newTask.innerHTML += `<img id="item-status" class="item-status" 
 		src="assets/img/done-s.png" alt="checkbox">`;
-		newTask.innerHTML += `<a class="item-text" 
+		newTask.innerHTML += `<a id="item-desctiption" class="item-text" 
 		href="#">${task.description}</a>`;
 		newTask.innerHTML += `<img id="delete-icon" class="item-delete" 
 		src="assets/img/remove-s.jpg" alt="delete button">`;
 	} else {
 		newTask.innerHTML += `<img id="item-status" class="item-status" 
 		src="assets/img/todo-s.png" alt="checkbox">`;
-		newTask.innerHTML += `<a class="item-text" 
-		href="#">${task.description}</a>`;
+		newTask.innerHTML += `<a id="item-desctiption" class="item-text" 
+		href="#/modify/${task.id}">${task.description}</a>`;
 		newTask.innerHTML += `<img id="delete-icon" class="item-delete" 
 		src="assets/img/remove-s.jpg" alt="delete button">`;
 	}
@@ -165,6 +173,7 @@ function checkAndDelTask() {
 	taskItems.forEach((item) => {
 		const checkbox = item.querySelector('#item-status');
 		const del = item.querySelector('#delete-icon');
+		
 		checkbox.onclick = (e) => {
 			const parentItem = e.currentTarget.parentNode;
 			completeTask(parentItem.dataset.id);
@@ -173,7 +182,6 @@ function checkAndDelTask() {
 		del.onclick = (e) => {
 			let task = e.currentTarget.parentNode;
 			removeTask(task.dataset.id);
-			console.log(task.dataset.id); //
 			showTaskList();
 		}
 	})
@@ -202,3 +210,27 @@ function removeTask(id) {
 	saveToNewData(tasks.filter((task) => task.id !== id));
 	location.reload(); 
 }
+
+function completeTask(id) {
+	itemsArray = itemsArray.map((item) =>{
+		if(item.id === id) {
+			item.isDone = true;
+		}
+		return item;
+	});
+	setItemToLocalStorage(); 
+	location.reload(); //add to setItemToLocalStorage()
+}
+
+// function updateTaskDesc(id, description) {
+// 	itemsArray = itemsArray.map((item) =>{
+// 		if(item.id === id) {
+// 			description: item.description;
+// 		}
+// 		return item;
+// 	});
+// 	setItemToLocalStorage(); 
+// 	location.reload(); //add to setItemToLocalStorage()
+// }
+
+// const desc = item.querySelector('#item-desctiption');
